f3d15c062c2e921086c24ceac78ce70da52ccb1a
AlexMunive <103223036+AlexMunive@users.noreply.github.com>
2023-02-05 11:19:13 -0500

    .

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Data.css'
-import TypeWriterEffect from 'react-typewriter-effect';
+import TypeWriterEffect from 'typewriter-effect';
 
 const Data = () => {
 
@@ -11,26 +11,38 @@ const Data = () => {
         <h3 className='home__subtitle'>Software Developer</h3>
         <h4 className='home__description'>I am a Full-Stack Web programmer with experience in: 
         <span className='home__specialty'>
-        {/* <TypeWriterEffect
-        textStyle={{
-            position: 'relative',
-            fontSize: '1rem',
-            fontWeight: '500',
-            marginBottom: '3rem',
-            marginLeft: '4px',
-            display: 'flex',
-            flexWrap: 'wrap',
+        <TypeWriterEffect
+        onInit={(typewriter)=> {
+  
+          typewriter
+           
+          .typeString("GeeksForGeeks")
+            
+          .pauseFor(1000)
+          .deleteAll()
+          .typeString("Welcomes You")
+          .start();
           }}
-          startDelay={100}
-          cursorColor="#3F3D56"
-          multiText={[
-            'React.js',
-            'Node.js',
+        // textStyle={{
+        //     position: 'relative',
+        //     fontSize: '1rem',
+        //     fontWeight: '500',
+        //     marginBottom: '3rem',
+        //     marginLeft: '4px',
+        //     display: 'flex',
+        //     flexWrap: 'wrap',
+        //   }}
+        //   startDelay={100}
+        //   cursorColor="#3F3D56"
+        //   multiText={[
+        //     'React.js',
+        //     'Node.js',
           
-          ]}
-          multiTextDelay={1000}
-          typeSpeed={300}
-          multiTextLoop/> */}
+        //   ]}
+        //   multiTextDelay={1000}
+        //   typeSpeed={300}
+        //   multiTextLoop
+          />
         </span></h4>
         <a href="#contac" className='button button--flex'>Contac <i className='bx bxs-navigation'></i></a>
     </div>
